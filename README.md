e0f835bb8437fe8f267215767fc91a56dc8d399a
Aydin Gurbanli <aydingurbanli@gmail.com>
2020-03-30 20:07:19 +0400

    Added disassembly function

--- README.md
+++ README.md
@@ -5,10 +5,29 @@ Basically, run install.sh as root. Tool will be installed as push2stack command.
 
 Usage:
 ```
-push2stack STRING
+usage: push2stack.py [-h] [-d] STRING
+
+positional arguments:
+  STRING
+
+optional arguments:
+  -h, --help         show this help message and exit
+  -d, --disassemble  disassemble push instructions with values
+
 ```
 Example:
 ```
 root@kali:~# push2stack "You have been hacked by gurbanli"
-\x68\x00\x68\x61\x6e\x6c\x69\x68\x67\x75\x72\x62\x68\x20\x62\x79\x20\x68\x63\x6b\x65\x64\x68\x6e\x20\x68\x61\x68\x20\x62\x65\x65\x68\x68\x61\x76\x65\x68\x59\x6f\x75\x20
+\x68\x20\x20\x20\x00\x68\x61\x6e\x6c\x69\x68\x67\x75\x72\x62\x68\x20\x62\x79\x20\x68\x63\x6b\x65\x64\x68\x6e\x20\x68\x61\x68\x20\x62\x65\x65\x68\x68\x61\x76\x65\x68\x59\x6f\x75\x20
+
+root@kali:~# push2stack "You have been hacked by gurbanli" -d
+PUSH 0x00202020
+PUSH 0x696c6e61
+PUSH 0x62727567
+PUSH 0x20796220
+PUSH 0x64656b63
+PUSH 0x6168206e
+PUSH 0x65656220
+PUSH 0x65766168
+PUSH 0x20756f59
 ```
--- push2stack.py
+++ push2stack.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
-import sys
+import sys, argparse
 
-def toHex(string):
+def toHex4Shellcode(string):
     lst = []
     for ch in string:
         hexvalue = hex(ord(ch)).replace('0x', '\\x')
@@ -12,26 +12,47 @@ def toHex(string):
     lst.append('\\x00')
     return lst
 
-string = ""
-if ( len(sys.argv) != 2 ):
-	print(f"Usage: {sys.argv[0]} STRING")
-	sys.exit(1)
+def toHex4Disassembly(string):
+    lst = []
+    for ch in string:
+        hexvalue = hex(ord(ch)).replace('0x', '')
+        lst.append(hexvalue)
+    if ( (len(lst)+1) % 4 != 0 ):
+        for i in range( 3-(len(lst) % 4)):
+            lst.append('20')
+    lst.append('00')
+    return lst
+
+def disassemble(ordered,string):
+    count = 0
+    newlist = []
+    for i in range(int(len(string)/4)+1):
+        newlist.append(ordered[count:count+4])
+        count = count + 4
+    disassembler_output = ""
+    for lst in newlist[::-1]:
+        disassembler_output += f'PUSH 0x{"".join(str(item) for item in lst[::-1])}\n'
+    return disassembler_output
+def generateShellcode(ordered, string):
+    count = 0
+    newlist = []
+    for i in range(int(len(string)/4)+1):
+        newlist.append(ordered[count:count+4])
+        count = count + 4
+    shellcode = ""
+    for lst in newlist[::-1]:
+        shellcode += "\\x68"
+        for item in lst:
+            shellcode += item
+    return shellcode
+
+parser = argparse.ArgumentParser()
+parser.add_argument('string', metavar="STRING", type=str)
+parser.add_argument('-d', '--disassemble', action='store_true', help='disassemble push instructions with values')
+args = parser.parse_args()
+
+string = args.string
+if args.disassemble:
+    print(disassemble(toHex4Disassembly(string),string))
 else:
-    string = sys.argv[1]
-
-ordered = toHex(string)
-
-count = 0
-
-newlist = []
-for i in range(int(len(string)/4)+1):
-    newlist.append(ordered[count:count+4])
-    count = count + 4
-
-shellcode = ""
-for lst in newlist[::-1]:
-    shellcode += "\\x68"
-    for item in lst:
-        shellcode += item
-
-print(shellcode)
+    print(generateShellcode(toHex4Shellcode(string),string))
